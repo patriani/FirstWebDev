75447733eb11f91ff65599cac73017944cb9e1cc
patriani <43487367+patriani@users.noreply.github.com>
2020-04-09 14:41:53 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$('.slider-principal').slick({
+
+    dots:true,
+    infinite:true,
+    speed: 300,
+    adaptativeHeight:true,
+    autoplay: true,
+    autoplaySpeed: 2000
+
+}); /* Ponto indica uma nova classe e '.slider-principal' eh um id */
